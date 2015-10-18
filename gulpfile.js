b4c992089afd95f19a1ca17aedf51b5c74a7ab3e
Andrey Pushkarev <fealaer@gmail.com>
2015-10-18 17:40:15 +0800

    Added build and clean gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,24 @@
-var gulp = require('gulp');
-var jasmine = require('gulp-jasmine');
+var gulp = require('gulp')
+  , jasmine = require('gulp-jasmine')
+  , browserify = require('gulp-browserify')
+  , clean = require('gulp-clean');
 
 gulp.task('watch', function () {
-  gulp.watch(['src/**/*.js', 'specs/**/*.js'], ['test']);
+  gulp.watch(['src/**/*.js', 'specs/**/*.js'], ['test', 'build']);
 });
 
 gulp.task('test', function () {
   return gulp.src('specs/**/*.js')
     .pipe(jasmine());
+});
+
+gulp.task('clean', function (cb) {
+  return gulp.src('public/*.js', {read: false})
+    .pipe(clean());
+});
+
+gulp.task('build', ['clean'], function () {
+  gulp.src('src/web-app.js')
+    .pipe(browserify({insertGlobals : true}))
+    .pipe(gulp.dest('./public/'));
 });
