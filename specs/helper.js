6d11f3151fb579dad9f64ffa415e73ab45cf9b4c
Andrey Pushkarev <fealaer@gmail.com>
2015-10-18 14:58:05 +0800

    Implemented a helper function parseInput

---
+++ specs/helper.js
@@ -0,0 +1,78 @@
+(function () {
+  'use strict';
+
+  var helper = require('../src/helper.js');
+
+  describe('Helper', function () {
+    it('should successfully parse a correct input', function () {
+      helper.parseInput('5 3\n1 1 E\nRFRFRFRF', function (err, res) {
+        expect(err).toBeNull();
+        expect(res.grid.size).toEqual({x: 5, y: 3});
+        expect(res.robot.status()).toEqual({x: 1, y: 1, orientation: 'E'});
+        expect(res.instructions).toEqual('RFRFRFRF'.split(''));
+      });
+
+      helper.parseInput('5 3\n3 2 N\nFRRFLLFFRRFLL', function (err, res) {
+        expect(err).toBeNull();
+        expect(res.grid.size).toEqual({x: 5, y: 3});
+        expect(res.robot.status()).toEqual({x: 3, y: 2, orientation: 'N'});
+        expect(res.instructions).toEqual('FRRFLLFFRRFLL'.split(''));
+      });
+
+    });
+
+    it('should failed to parse an empty or not full input', function () {
+      helper.parseInput('', function (err, res) {
+        expect(res).toBeNull();
+        expect(err).toEqual('ERROR: Input is not correct! REASON: It should be exactly 3 lines of input data\n');
+      });
+
+      helper.parseInput('5 3\n1 1 E', function (err, res) {
+        expect(res).toBeNull();
+        expect(err).toEqual('ERROR: Input is not correct! REASON: It should be exactly 3 lines of input data\n5 3\n1 1 E');
+      });
+    });
+
+    it('should failed to parse input with an incorrect grid data', function () {
+      helper.parseInput('5 3 4\n3 2 N\nFRRF', function (err, res) {
+        expect(res).toBeNull();
+        expect(err).toEqual('ERROR: Input is not correct! REASON: The first line of input should be a grid size.\nA grid size consists of two integers specifying max values of X and Y the upper-right coordinates of the rectangular world\n5 3 4');
+      });
+
+      helper.parseInput('5 \n3 2 N\nFRRF', function (err, res) {
+        expect(res).toBeNull();
+        expect(err).toEqual('ERROR: Input is not correct! REASON: The first line of input should be a grid size.\nA grid size consists of two integers specifying max values of X and Y the upper-right coordinates of the rectangular world\n5 ');
+      });
+    });
+
+    it('should failed to parse input with an incorrect robot position data', function () {
+      helper.parseInput('5 3\n3 2\nFRRF', function (err, res) {
+        expect(res).toBeNull();
+        expect(err).toEqual('ERROR: Input is not correct! REASON: The second line of input should be a robot position.\nA position consists of two integers specifying the initial coordinates of the robot and an orientation (N, S, E, W), all separated by whitespace on one line.\n3 2');
+      });
+
+      helper.parseInput('5 3\n3 N\nFRRF', function (err, res) {
+        expect(res).toBeNull();
+        expect(err).toEqual('ERROR: Input is not correct! REASON: The second line of input should be a robot position.\nA position consists of two integers specifying the initial coordinates of the robot and an orientation (N, S, E, W), all separated by whitespace on one line.\n3 N');
+      });
+
+      helper.parseInput('5 3\n3 2 G\nFRRF', function (err, res) {
+        expect(res).toBeNull();
+        expect(err).toEqual('ERROR: Input is not correct! REASON: The second line of input should be a robot position.\nA position consists of two integers specifying the initial coordinates of the robot and an orientation (N, S, E, W), all separated by whitespace on one line.\n3 2 G');
+      });
+    });
+
+    it('should failed to parse input with an incorrect robot instructions data', function () {
+      helper.parseInput('5 3\n3 2 N\n', function (err, res) {
+        expect(res).toBeNull();
+        expect(err).toEqual('ERROR: Input is not correct! REASON: The third line of input should be robot instructions from the Earth.\nA robot instruction is a string of the letters "L", "R", and "F" on one line.\n');
+      });
+
+      helper.parseInput('5 3\n3 2 N\nGFLR', function (err, res) {
+        expect(res).toBeNull();
+        expect(err).toEqual('ERROR: Input is not correct! REASON: The third line of input should be robot instructions from the Earth.\nA robot instruction is a string of the letters "L", "R", and "F" on one line.\nGFLR');
+      });
+    });
+  });
+
+})();
---
+++ src/helper.js
@@ -0,0 +1,59 @@
+(function () {
+  'use strict';
+
+  var Grid = require('./grid.js');
+  var Robot = require('./robot.js');
+
+  module.exports = {
+    parseInput: function (input, callback) {
+      var lines = input.split('\n');
+      var res = {};
+
+      function error(info, given) {
+        callback('ERROR: Input is not correct! REASON: ' + info + '\n' + given, null);
+        return false;
+      }
+
+      function initialiseGrid(line) {
+        var size = line.split(' ');
+        var x, y;
+        if (size.length !== 2 || isNaN(x = parseInt(size[0])) || isNaN(y = parseInt(size[1]))) {
+          return error('The first line of input should be a grid size.\nA grid size consists of two integers specifying max values of X and Y the upper-right coordinates of the rectangular world', line);
+        }
+        return new Grid(x, y);
+      }
+
+      function initialiseRobot(line) {
+        var position = line.split(' ');
+        var x, y;
+        if (position.length !== 3 || isNaN(x = parseInt(position[0])) || isNaN(y = parseInt(position[1])) || !~'NESW'.indexOf(position[2])) {
+          return error('The second line of input should be a robot position.\nA position consists of two integers specifying the initial coordinates of the robot and an orientation (N, S, E, W), all separated by whitespace on one line.', line);
+        }
+        return new Robot(x, y, position[2]);
+      }
+
+      function initialiseInstructions(line) {
+        function isIncorrectInstruction(instruction) {
+          return !~'LRF'.indexOf(instruction);
+        }
+
+        var instructions = line.split('');
+
+        if (!instructions.length || (instructions.filter(isIncorrectInstruction)).length) {
+          return error('The third line of input should be robot instructions from the Earth.\nA robot instruction is a string of the letters "L", "R", and "F" on one line.', line);
+        }
+        return instructions;
+      }
+
+      if (lines.length !== 3) {
+        return error('It should be exactly 3 lines of input data', input);
+      } else {
+        if ((res.grid = initialiseGrid(lines[0])) &&
+          (res.robot = initialiseRobot(lines[1])) &&
+          (res.instructions = initialiseInstructions(lines[2]))) {
+          return callback(null, res)
+        }
+      }
+    }
+  };
+})();
